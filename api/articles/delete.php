b6bc2d75a77a5a1636806126baaedfdf62377314
lamia <lamia.ziachi@mmibordeaux.com>
2023-02-10 12:23:30 +0100

    corrections

--- api/articles/delete.php
+++ api/articles/delete.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/../../config.php';;
 $numArt = sql_escape($_POST["numArt"]);
 
 sql_delete("COMMENT","numArt = $numArt");
+sql_delete("MOTCLEARTICLE","numArt = $numArt");
 sql_delete("LIKEART","numArt = $numArt");
 sql_delete("ARTICLE","numArt = $numArt");
 
